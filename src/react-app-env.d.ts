6b85671083cfc713ca83cf89918ba4834d0d123e
crissdev <criss.trifan@gmail.com>
2019-11-21 09:53:52 +0200

    enable hot reloading
    https://easy-peasy.now.sh/docs/recipes/hot-reloading.html

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+
+declare interface NodeModule {
+  hot: {
+    accept(path?: string, fn: () => void, callback?: () => void): void;
+  }
+}
--- src/store.ts
+++ src/store.ts
@@ -3,4 +3,14 @@ import { storeModel } from './model';
 
 const store = createStore(storeModel, { devTools: process.env.NODE_ENV === 'development' });
 
+// Wrapping dev only code like this normally gets stripped out by bundlers
+// such as Webpack when creating a production build.
+if (process.env.NODE_ENV === 'development') {
+  if (module.hot) {
+    module.hot.accept('./model', () => {
+      store.reconfigure(storeModel); // 👈 Here is the magic
+    });
+  }
+}
+
 export default store;

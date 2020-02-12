487eb3ab08a5e702b7615332917f7d17a9a9deaa
crissdev <criss.trifan@gmail.com>
2020-02-12 12:39:55 +0200

    add tests for posts model

---
+++ src/__tests__/posts-model.spec.ts
@@ -0,0 +1,31 @@
+import { createStore } from 'easy-peasy';
+import { postsModel, Post } from '../model/posts-model';
+
+describe('Posts', () => {
+  it('should be initialized', () => {
+    // arrange
+    const store = createStore(postsModel, { initialState: { posts: [] } });
+
+    // assert
+    expect(store.getState().posts).toEqual([]);
+  });
+
+  it('should like post', () => {
+    // arrange
+    const post: Post = {
+      caption: 'Lunch #hamont',
+      likes: 8,
+      id: 'BAcyDyQwcXX',
+      src: 'https://picsum.photos/400/400/?image=64',
+    };
+    const store = createStore(postsModel, {
+      initialState: { posts: [post] },
+    });
+
+    // act
+    store.getActions().likePost('BAcyDyQwcXX');
+
+    // assert
+    expect(store.getState().posts).toEqual([{ ...post, likes: 9 }]);
+  });
+});
